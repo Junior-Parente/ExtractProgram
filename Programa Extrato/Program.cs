2d9a80320fd22be6e7d6ea6d51d58ea032cc21a4
Junior Parente <juniorausec@gmail.com>
2021-05-19 22:54:04 -0300

    first commit

---
+++ Programa Extrato/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+            /* Programa para extrato - exemplo Bancário
+            - Saldo Inicial - 1.000,00
+            - Visualizar Saldo
+            - Fazer operacoes (debito, credito)
+            - Realizar consulta do extrato
+            - Realizar upload no Github
+            */
+
+namespace Programa_Extrato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
